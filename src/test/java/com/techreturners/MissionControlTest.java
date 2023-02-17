82e03b89d3d475a8def14e6909e40a2f66405d34
ibrahim rahman <heemee@gmail.com>
2023-02-17 08:27:58 +0000

    Added parametrised testing

--- src/test/java/com/techreturners/MissionControlTest.java
+++ src/test/java/com/techreturners/MissionControlTest.java
@@ -1,7 +1,8 @@
 package com.techreturners;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.awt.Point;
 
@@ -50,6 +51,54 @@ public class MissionControlTest {
         char direction = 'E';
         assertEquals( direction, control.MarsRoverDirection());
 
+    }
+
+    class YouAreHere {
+        public Point p;
+        public char direction = ' ';
+    }
+
+    public YouAreHere ConvertInputData(String s) {
+        YouAreHere here = new YouAreHere();
+
+        String[] strNums;
+        strNums = s.split(" ");
+
+        // Convert into a Point
+        int one = Integer.parseInt(strNums[0]);
+        int two = Integer.parseInt(strNums[1]);
+        here.p = new Point(one ,two );
+
+        // Get the direction pointed
+        if (strNums.length > 2)
+            here.direction = strNums[2].charAt(0);
+
+        return here;
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources="/MelodyMarsMissionTest.csv", numLinesToSkip = 1)
+    public void MelodyMarsMissionParametrisedTest(String input1, String input2, String input3, String expected) {
+
+        YouAreHere here = ConvertInputData(input1);
+
+        Plateau gridMap = new GridMap(here.p);
+        ControlRoom control = new MissionControl(gridMap);
+
+
+        // Setup Position (of Mars Rover)
+        here = ConvertInputData(input2);
+        Point loc = new Point(here.p);
+        MarsVehicle rover = new Rover();
+        control.AddVehicle(here.p, here.direction);
+
+        // Instruct Rover to move to a new position
+        control.MoveRover(input3);
+
+        here = ConvertInputData(expected);
+        assertEquals( here.p, control.MarsRoverPosition());
+        assertEquals( here.direction, control.MarsRoverDirection());
     }
 
 }
